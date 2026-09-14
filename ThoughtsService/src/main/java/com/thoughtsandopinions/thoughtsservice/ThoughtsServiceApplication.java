0690fa2b7c0e464dab4ba678bd5cf6f7f4d63db7
Phanidhar reddy guttikonda <97611031+phanidharguttikonda0@users.noreply.github.com>
2026-09-14 22:39:25 +0530

    implemented thoughts proto

---
+++ ThoughtsService/src/main/java/com/thoughtsandopinions/thoughtsservice/ThoughtsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtsandopinions.thoughtsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtsServiceApplication.class, args);
+	}
+
+}
---
+++ ThoughtsService/src/test/java/com/thoughtsandopinions/thoughtsservice/ThoughtsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtsandopinions.thoughtsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
