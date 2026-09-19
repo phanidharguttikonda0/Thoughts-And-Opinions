47cadf0d914671a7a78340b05a0aad3e4a397621
Phanidhar reddy guttikonda <97611031+phanidharguttikonda0@users.noreply.github.com>
2026-09-20 00:29:07 +0530

    made authentication controllers also non blocking code

--- APIGateway/src/main/java/com/thoughtsandopinions/apigateway/controller/AuthenticationController.java
+++ APIGateway/src/main/java/com/thoughtsandopinions/apigateway/controller/AuthenticationController.java
@@ -5,11 +5,12 @@ import com.thoughtsandopinions.apigateway.dto.api.ResponseDTO;
 import com.thoughtsandopinions.apigateway.dto.api.SignInDTO;
 import com.thoughtsandopinions.apigateway.dto.api.SignUpDTO;
 import com.thoughtsandopinions.apigateway.gRPC.IdentityServiceGrpcHandler;
-import identity.AuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,45 +24,49 @@ public class AuthenticationController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<ResponseDTO<AuthenticationResponse>> signUp(@RequestBody SignUpDTO request) {
-        AuthResponse response = identityServiceGrpcHandler.signUp(
+    public Mono<ResponseEntity<ResponseDTO<AuthenticationResponse>>> signUp(@RequestBody SignUpDTO request) {
+        // We cannot unwrap the mono , the spring webflux unwraps it when the asynchronous operation completes
+        return Mono.fromCallable(() -> identityServiceGrpcHandler.signUp(
                 request.username(),
                 request.email(),
                 request.password()
-        );
+        ))
+        .subscribeOn(Schedulers.boundedElastic())
+        .map(gRPCResponse -> {
+            AuthenticationResponse authResponse = AuthenticationResponse.builder()
+                    .token(gRPCResponse.getJwtToken())
+                    .build();
 
-        AuthenticationResponse authResponse = AuthenticationResponse.builder()
-                .token(response.getJwtToken())
-                .build();
+            ResponseDTO<AuthenticationResponse> responseDTO = ResponseDTO.<AuthenticationResponse>builder()
+                    .success(true)
+                    .message("User signed up successfully")
+                    .data(authResponse)
+                    .build();
 
-        ResponseDTO<AuthenticationResponse> responseDTO = ResponseDTO.<AuthenticationResponse>builder()
-                .success(true)
-                .message("User signed up successfully")
-                .data(authResponse)
-                .build();
-
-        // need to pass the userId , name, username and profilePic Url to Thoughts Service to store it in users_cache
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+            // need to pass the userId , name, username and profilePic Url to Thoughts Service to store it in users_cache
+            return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+        });
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<ResponseDTO<AuthenticationResponse>> signIn(@RequestBody SignInDTO request) {
-        AuthResponse response = identityServiceGrpcHandler.signIn(
+    public Mono<ResponseEntity<ResponseDTO<AuthenticationResponse>>> signIn(@RequestBody SignInDTO request) {
+        return Mono.fromCallable(() -> identityServiceGrpcHandler.signIn(
                 request.username(), 
                 request.password()
-        );
+        ))
+        .subscribeOn(Schedulers.boundedElastic())
+        .map(gRPCResponse -> {
+            AuthenticationResponse authResponse = AuthenticationResponse.builder()
+                    .token(gRPCResponse.getJwtToken())
+                    .build();
 
-        AuthenticationResponse authResponse = AuthenticationResponse.builder()
-                .token(response.getJwtToken())
-                .build();
+            ResponseDTO<AuthenticationResponse> responseDTO = ResponseDTO.<AuthenticationResponse>builder()
+                    .success(true)
+                    .message("User signed in successfully")
+                    .data(authResponse)
+                    .build();
 
-        ResponseDTO<AuthenticationResponse> responseDTO = ResponseDTO.<AuthenticationResponse>builder()
-                .success(true)
-                .message("User signed in successfully")
-                .data(authResponse)
-                .build();
-
-        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+        });
     }
 }
